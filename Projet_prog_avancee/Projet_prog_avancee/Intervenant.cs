90ae9de6232b8dabbd6cb843ccb94c6c53d5c9a2
Jadounix <jademariepetit@yahoo.fr>
2020-03-30 11:55:11 +0200

    Refonte classes

--- Projet_prog_avancee/Projet_prog_avancee/Intervenant.cs
+++ Projet_prog_avancee/Projet_prog_avancee/Intervenant.cs
@@ -6,25 +6,26 @@ using System.Threading.Tasks;
 
 namespace Projet_prog_avancee
 {
-    abstract class Intervenant
+    class Intervenant
     {
         protected string _nom { get; set; }
         protected string _prenom { get; set; }
         protected string _role { get; set; }
+        protected int _promo { get; set; }
+        protected string _anneeScolaire { get; set; }
+        protected List<string> _listeMatieres;
+
 
         //Constructeur
-        public Intervenant(string nom, string prenom, string role)
+        public Intervenant(string nom, string prenom, string role, int promo, string anneeScolaire)
         {
             _nom = nom;
             _prenom = prenom;
             _role = role;
+            _promo = promo;
+            _anneeScolaire = anneeScolaire;
+            _listeMatieres = new List<string>();
         }
 
-        //Méthode ToString
-        public override string ToString()
-        {
-            string chS = "Cet intervenant se nomme : " + _prenom + " " + _nom + " et a pour role " + _role;
-            return chS;
-        }
     }
 }
